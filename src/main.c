76421c74e010a3e43aea872598131140ec831f82
zhunhe <xuzhunhe@gmail.com>
2022-04-27 20:50:04 +0900

    etc: Add 3 arguments in main()
    - argc, argv, envp

--- src/main.c
+++ src/main.c
@@ -6,14 +6,18 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 15:58:28 by juhur             #+#    #+#             */
-/*   Updated: 2022/04/08 21:08:02 by juhur            ###   ########.fr       */
+/*   Updated: 2022/04/27 20:49:56 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
+	(void)argc;
+	(void)argv;
+	// TODO: envp -> linked list
+	(void)envp;
 	echoctl_off();
 	print_prompt();
 	return (0);
