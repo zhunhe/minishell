8abfa91540541c250ee81855b99f91ccb93a936a
zhunhe <xuzhunhe@gmail.com>
2022-04-30 11:21:16 +0900

    refactor: Change data type
    - int -> enum type in s_node

--- include/ast.h
+++ include/ast.h
@@ -6,24 +6,12 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 14:19:41 by juhur             #+#    #+#             */
-/*   Updated: 2022/04/27 15:06:49 by juhur            ###   ########.fr       */
+/*   Updated: 2022/04/30 11:21:03 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef AST_H
 # define AST_H
-
-typedef struct s_node
-{
-	struct s_node	*left;
-	struct s_node	*right;
-	int				type;
-}	t_node;
-
-typedef struct s_ast
-{
-	struct s_node	*root;
-}	t_ast;
 
 typedef enum e_node_type
 {
@@ -34,6 +22,18 @@ typedef enum e_node_type
 	NODE_CMD,
 	NODE_ETC
 }	t_node_type;
+
+typedef struct s_node
+{
+	struct s_node	*left;
+	struct s_node	*right;
+	t_node_type		type;
+}	t_node;
+
+typedef struct s_ast
+{
+	struct s_node	*root;
+}	t_ast;
 
 t_node_type	get_type(char *str);
 t_node		*create_node(char *data);
