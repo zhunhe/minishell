1a3485036b6ad530d7a87b66713038d56e5cc45a
zhunhe <xuzhunhe@gmail.com>
2022-05-08 13:27:52 +0900

    fix: Fix ast.h to avoid conflict
    - void create_ast(t_ast *ast, char *s)
    -> t_ast *create_ast(char **ss)

--- include/ast.h
+++ include/ast.h
@@ -6,7 +6,7 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 14:19:41 by juhur             #+#    #+#             */
-/*   Updated: 2022/05/06 00:39:27 by juhur            ###   ########.fr       */
+/*   Updated: 2022/05/08 13:27:25 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,6 @@ typedef struct s_ast
 t_node_type	get_type(char *str);
 t_node		*create_node(char *data);
 t_node		*get_last_left_node(t_ast *ast);
-void		create_ast(t_ast *ast, char *s);
+t_ast		*create_ast(char **ss);
 
 #endif
