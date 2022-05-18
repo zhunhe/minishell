703b26d94009e23e52a8b9bd2fa8aa03e4992cb4
42hena <somnium1108@gmail.com>
2022-05-18 19:08:34 +0900

    Refactor : check env
    - check norm
    - change str function to _str(my)

--- src/builtin/ft_env.c
+++ src/builtin/ft_env.c
@@ -1,19 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_env.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hena <hena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/18 18:50:36 by hena              #+#    #+#             */
+/*   Updated: 2022/05/18 18:50:48 by hena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include "minishell.h"
 #include "parse.h"
+#include "util.h"
 
-extern t_minishell g_minishell;
+extern t_minishell	g_minishell;
 
 static void	ft_print_env(char *key, char *value)
 {
-	write(1, key, strlen(key)); //TODO:: strlen
+	_putstr_fd(key, 1);
 	write(1, "=", 1);
-	write(1, value, strlen(value)); //TODO:: strlen
+	_putstr_fd(value, 1);
 	write(1, "\n", 1);
 }
-
 
 void	ft_env(t_exec *data, int pipe_flag)
 {
@@ -25,8 +37,7 @@ void	ft_env(t_exec *data, int pipe_flag)
 	argv++;
 	if (*argv)
 	{
-		write(2, "env must be one argv", 20);//TODO:: stlen
-		write(2, "\n", 1);
+		_putendl_fd("env must be one argv", 2);
 		exit(1);
 	}
 	envpl = g_minishell.envp;
@@ -41,63 +52,3 @@ void	ft_env(t_exec *data, int pipe_flag)
 	if (pipe_flag)
 		exit(0);
 }
-
-#if 0
-
-t_list *create_l()
-{
-	t_list	*new;
-
-	new = (t_list *)malloc(sizeof(t_list));
-	new->data = 0;
-	new->next = 0;
-	return (new);
-}
-
-t_envp *create_e(char *key, char *value)
-{
-	t_envp	*new;
-
-	new = (t_envp *)malloc(sizeof(t_envp));
-	if (key)
-		new->key = strdup(key);
-	else
-		new->key = NULL;
-	if (value)
-		new->value = strdup(value);
-	else
-		new->value = NULL;
-	return (new);
-}
-int	main(void)
-{
-	g_minishell.envp = NULL;
-
-	t_list	*envpl;
-	t_envp	*envp;
-	t_exec	*data;
-	envpl = create_l();
-	envp = create_e("a", NULL);
-	g_minishell.envp = envpl;
-	envpl->data = (void *)envp;
-
-	envpl = create_l();
-	envp = create_e("b", "test");
-	g_minishell.envp->next = envpl;
-	envpl->data = envp;
-
-	envpl = create_l();
-	envp = create_e("c", "hi");
-	g_minishell.envp->next->next = envpl;
-	envpl->data = envp;
-
-	data = (t_exec*)malloc(sizeof(t_exec));
-	data->cmd = strdup("env");
-	data->cmd_path = strdup("/bin/env");
-	data->cmd_argv = (char **)malloc(sizeof(char *) * 4);
-	data->cmd_argv[0] = strdup("env");
-	data->cmd_argv[1] =strdup("env");
-	data->cmd_argv[2] = NULL;
-	ft_env(data, 0);
-}
-#endif
