4151384314dff73bc82ef5bfc1e482c159813e73
zhunhe <xuzhunhe@gmail.com>
2022-05-22 16:46:02 +0900

    fix: Print lf when press ctrl+C in heredoc

--- src/pipe_signal/signal_here.c
+++ src/pipe_signal/signal_here.c
@@ -6,7 +6,7 @@
 /*   By: hena <hena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 02:25:15 by hena              #+#    #+#             */
-/*   Updated: 2022/05/22 13:09:25 by hena             ###   ########.fr       */
+/*   Updated: 2022/05/22 16:18:38 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	sig_heredoc_handler(int signal)
 {
 	if (signal != SIGINT)
 		return ;
+	write(1, "\n", 1);
 	dup2(0, 200);
 	close(0);
 	g_minishell.exit_status = 1;
